745482299a0f82cf80c6c41d203aacc1ddb00ee0
Mafin <optimus2004r@gmail.com>
2024-12-20 17:25:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr10workmb/MainActivity.kt
@@ -0,0 +1,168 @@
+package com.example.pr10workmb
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    var imageUrl by remember { mutableStateOf(TextFieldValue()) }
+    var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+
+    val imageUrls = listOf(
+        "https://repository-images.githubusercontent.com/408963819/95a010f2-62fd-4709-a2c2-d073317c70ff",
+        "https://example.com/image10.jpg",
+        "aboba",
+        "aboba",
+        "aboba",
+        "aboba",
+        "aboba",
+
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        imageBitmap?.let { bitmap ->
+            Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier.size(400.dp)
+            )
+        }
+        TextField(
+            value = imageUrl,
+            onValueChange = { imageUrl = it },
+            label = { Text("Ввести ссылку") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            if (imageUrl.text.isNotEmpty()) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val bitmap = downloadImage(imageUrl.text)
+                    if (bitmap != null) {
+                        withContext(Dispatchers.Main) {
+                            imageBitmap = bitmap
+                        }
+                        saveImageToDisk(bitmap, context)
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(context, "Ошибка загрузки изображения", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }) {
+            Text("Загрузить изображение")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        LazyColumn(modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+        ) {
+            items(imageUrls) { url ->
+                Text(text = url, modifier = Modifier.padding(8.dp))
+            }
+        }
+    }
+}
+
+private suspend fun downloadImage(imageUrl: String): Bitmap? {
+    return withContext(Dispatchers.IO) {
+        try {
+            val url = URL(imageUrl)
+            val connection = url.openConnection()
+            connection.doInput = true
+            connection.connect()
+            val input = connection.getInputStream()
+            BitmapFactory.decodeStream(input)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
+
+private suspend fun saveImageToDisk(bitmap: Bitmap, context: Context) {
+    withContext(Dispatchers.IO) {
+        try {
+            val file = File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                "downloaded_image.jpg"
+            )
+            FileOutputStream(file).use { outputStream ->
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+            }
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "Изображение сохранено", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "Ошибка сохранения изображения", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
